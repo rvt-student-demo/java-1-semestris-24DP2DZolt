195a428e5ba5708c95db1b0ed8bf83d5362660a4
Dominiks Žoltikovs <A242011DZ@rvt.lv>
2025-09-23 13:14:26 +0300

    Uzdevums "Pāra vai Nepāra skaitlis"

---
+++ src/main/java/rvt/para_skaitli.java
@@ -0,0 +1,24 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class para_skaitli {
+
+    public static void main(String args[])    {
+         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number: ");
+
+        int skaitlis = Integer.valueOf(scanner.nextLine());
+
+        if (skaitlis % 2 == 0){
+            System.out.println("Number " + skaitlis + " is even.");
+    } else{
+        System.out.println("Number " + skaitlis + " is odd.");
+    }
+    }
+
+} 
+    
+
